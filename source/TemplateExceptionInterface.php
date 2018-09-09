b70cda5c6d03080f187db051177065723a2a8d24
Алексей Арх <info@arh.one>
2018-09-09 15:14:11 +0300

    dev

---
+++ source/TemplateExceptionInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace arhone\templating;
+
+/**
+ * Interface TemplateExceptionInterface
+ * @package arhone\templating
+ * @author Алексей Арх <info@arh.one>
+ */
+interface TemplateExceptionInterface {}
